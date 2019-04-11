29b0ae484243bc7d08b14e7daf0d9f48401b971c
dheivids <davidmarcos_s@yahoo.es>
2019-04-11 12:35:46 +0200

    variables

---
+++ 11-var/js/app.js
@@ -0,0 +1,8 @@
+/* las variables son los elementos que utilizamos para almacenar informacion, es "como" un nombre que se asigna un valor */
+/* crear variable: */
+let x = 44
+let y = 5
+let suma 
+suma = x + y
+console.log (suma) 
+/* console.dir (suma/* ) *//*  las dos opciones pintan en consola  */
