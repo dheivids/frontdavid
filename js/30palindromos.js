467615b15639283207321ea5558a7b1d5ea14c5f
dheivids <davidmarcos_s@yahoo.es>
2019-05-08 10:14:54 +0200

    dni y palindromos

--- js/30palindromos.js
+++ js/30palindromos.js
@@ -1,46 +1,42 @@
 /**
- * comporbar si una funcion es un palindromo
- * se lee igual en ambos sentidos sin contar los espacios
- * EJEMPLO:
- * Alli va Ramon y no maravilla.
- * Amad a la dama.
+ * Comprobar si una frase SIN ACENTOS es un palíndromo
+ * Se lee igual en ambos setidos, sin contar los espacios
+ * por ejemplo
+ * La ruta nos aportó otro paso natural
+ * Atar a la rata...
  */
 
  /**
   * function isPali
-  * @param cadena : string
-  * @returns : boolean
+  * @param cadena: string
+  * @return: boolean
   */
 
-  function isPali(cadena = '') {
-      let r = true
-      let cadenaSinEspacios = cadena.split(' ').join('').toLowerCase()
-      console.log(cadenaSinEspacios)
-      let cadenaInversa = cadenaSinEspacios.split('').reverse().join('')
-      console.log(cadenaInversa)
-      if(cadenaSinEspacios !== cadenaInversa) {
-          r = false
-      }
-      return r
-  }
+ function isPali(cadena = '') {
+    let r = true
+    let cadenaSinEspacios = cadena.split(' ').join('').toLowerCase()
+    // console.log(cadenaSinEspacios)
+    let cadenaInversa = cadenaSinEspacios.split('').reverse().join('')
+    // console.log(cadenaInversa)
+    if (cadenaSinEspacios !== cadenaInversa ) {
+        r = false
+    }
+    return r
+}
 
-  function isPali2(cadena = '') {
-      let r = true
-      let cadenaInversa = cadena
-  }
-
-
-
+function isPaliShort(cadena = '') {
+return cadena.split(' ').join('').toLowerCase() === cadena.split(' ').join('').toLowerCase().split('').reverse().join('') ? true: false
+}
 
 (function () {
-    let textos = [
-        'Esto no es un palindromo',
-        'La ruta nos aporto otro paso natural',
-        'Atar a la rata'
-    ]
-    for (let i = 0; i < textos.length; i++) {
-        const texto = textos[i];
-        console.log(texto)
-        console.log(isPaliShort(texto))
-    }
-}) ()
+let textos = [
+    'Esto no es un palíndromo',
+    'La ruta nos aporto otro paso natural',
+    'Atar a la rata'
+]
+for (let i = 0; i < textos.length; i++) {
+    const texto = textos[i];
+    console.log(texto)
+    console.log(isPaliShort(texto))
+}
+})()
