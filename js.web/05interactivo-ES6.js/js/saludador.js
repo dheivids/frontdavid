f0e87e8c62ca4ecac0272b4560334277947bb579
dheivids <davidmarcos_s@yahoo.es>
2019-05-15 11:34:20 +0200

    saludador bootstrap

--- js.web/05interactivo-ES6.js/js/saludador.js
+++ js.web/05interactivo-ES6.js/js/saludador.js
@@ -1,44 +1,39 @@
-export class  Saludador {
+export class Saludador {
+    constructor() {
+        this.aQuien = ''
+        this.msg = [`Hola `, `Adios `]
 
-constructor(){
-    this.aQuien = ''
-    this.msg = [`hola `, `adios `]
-    
+        /** Seleccionar nodos del DOM */
+        this.inNombre = document.querySelector('#in-nombre')
+        this.btnSaludar = document.querySelector('#btn-saludar')
+        this.btnDespedirse = document.querySelector('#btn-despedirse')
+        this.btnBorrar = document.querySelector('#btn-borrar')
+        this.output = document.querySelector('#output')
+        
+        /**Asignar manejadores a los nodos */
+        this.btnSaludar.addEventListener('click', this.saludar.bind(this))
+        this.btnDespedirse.addEventListener('click', this.despedirse.bind(this))
+        this.btnBorrar.addEventListener('click', this.borrar.bind(this))
+    }
 
-///seleccionar nodos del DOM
-    this.inNombre = document.querySelector('#in-nombre')
-    this.btnSaludar = document.querySelector('#btn-saludar')
-    this.btnDespedirse = document.querySelector('#btn-despedirse')
-    this.output = document.querySelector("#output")
-    this.btnClear = document.querySelector('#btn-clear')
+    saludar() {
+        if (this.inNombre.value) {
+            this.aQuien = '<b>' + this.inNombre.value + '</b>'
+            // console.log(this.msg[0] + this.aQuien)
+            this.output.innerHTML = this.msg[0] + this.aQuien
+        }
+    }
 
-//asignar manejadores a los nodos///
-    this.btnSaludar.addEventListener('click', this.saludar.bind(this))
-    this.btnDespedirse.addEventListener('click', this.despedirse.bind(this))
-    this.btnClear.addEventListener('click', this.clear.bind(this))
+    despedirse() {
+        if (this.inNombre.value) {
+            this.aQuien = '<b>' + this.inNombre.value + '</b>'
+            // console.log(this.msg[1] + this.aQuien)
+            this.output.innerHTML = this.msg[1] + this.aQuien            
+        }
+    }
 
-}
-saludar(){
-    if(this.inNombre.value){
-        this.aQuien = '<b>' + this.inNombre.value + '</b>'
-        this.output.innerHTML =this.msg[0] + this.aQuien
-}
-
-
-  /*   }
-     this.aQuien = '<b>'+ this.inNombre.value + '</b>' 
-     console.log(this.msg[0] + this.aQuien) 
-    this.output.innerHTML =this.msg[0] + this.aQuien
-} */
-despedirse()
-    if(this.inNombre.value){
-        this.aQuien = '<b>' + this.inNombre.value + '</b>'
-        this.output.innerHTML = this.msg[1] + this.aQuien
-
+    borrar() {
+        this.output.innerHTML = ''
+        this.inNombre.value = ''
     }
 }
-
-clear(){
-    this.output.innerHTML = ''
-    this.inNombre.value = ''
-} }
--- js.web/06interactivo.js/js/saludador.js
+++ js.web/06interactivo.js/js/saludador.js
@@ -1,32 +1,45 @@
-export function main(){
 
-     let aQuien = ''
-     let msg = [`hola `, `adios `]
-    
+export function main () {
 
-///seleccionar nodos del DOM
+    let aQuien = ''
+    let msg = [`Hola `, `Adios `]
+
+    /** Seleccionar nodos del DOM */
     let inNombre = document.querySelector('#in-nombre')
     let btnSaludar = document.querySelector('#btn-saludar')
     let btnDespedirse = document.querySelector('#btn-despedirse')
-
-
-    function saludar(){
-             aQuien = inNombre.value
-             console.log(msg[0] + aQuien)
-         }
-
-
-         btnSaludar.addEventListener('click', saludar )
-         btnDespedirse.addEventListener('click', despedirse )
-
-     
-
-         function despedirse(){
+    let btnBorrar = document.querySelector('#btn-borrar')
+    let output = document.querySelector('#output')
+    
+    /**Asignar manejadores a los nodos */
+    btnSaludar.addEventListener('click', saludar)
+    /* btnSaludar.addEventListener('click', () => {
             aQuien = inNombre.value
-            console.log(msg[1] + aQuien)
+            console.log(msg[0] + aQuien)
+    }) */
+    btnDespedirse.addEventListener('click', despedirse)
+    btnBorrar.addEventListener('click', borrar)
+
+    function saludar () {
+        if (inNombre.value) {
+            aQuien = '<b>' + inNombre.value + '</b>'
+            // console.log(msg[0] + aQuien)
+            output.innerHTML = msg[0] + aQuien
         }
+    }
+
+    function despedirse() {
+        if (inNombre.value) {
+            aQuien = '<b>' + inNombre.value + '</b>'
+            // console.log(msg[1] + aQuien)
+            output.innerHTML = msg[1] + aQuien 
+        }
+    }
+
+    function borrar() {
+        output.innerHTML = ''
+        inNombre.value = ''
+    }
 }
-
-
 
 
