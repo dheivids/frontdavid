b9d7d38a96959259016626bdb28453891266a7bc
dheivids <davidmarcos_s@yahoo.es>
2019-06-12 09:16:27 +0200

    ok

--- js.web/19fotos/js/bootstrap.js
+++ js.web/19fotos/js/bootstrap.js
@@ -6,4 +6,3 @@ Es buena practica crear un módulo aparte e importar la función principal
 
 // Proceso ASINCRONO
 document.addEventListener('DOMContentLoaded', controller)
-
--- js.web/19fotos/js/controller.js
+++ js.web/19fotos/js/controller.js
@@ -1,3 +1,4 @@
+    
 import { ajax } from "./ajax.js";
 
 export function controller() {
